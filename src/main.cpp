ead2ca23c3367f92c4d151436ae9f98f01f537b1
Shweta Racharla <99076323+racharlashweta@users.noreply.github.com>
2026-04-01 11:53:40 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -9,56 +9,49 @@
 #include <netdb.h>
 
 int main(int argc, char **argv) {
-    // Flush after every std::cout / std::cerr
+    // Standard setup for CodeCrafters
     std::setvbuf(stdout, NULL, _IOLBF, 0);
     std::setvbuf(stderr, NULL, _IOLBF, 0);
     
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        std::cerr << "Failed to create server socket\n";
-        return 1;
-    }
-
-    // This helps avoid "Address already in use" errors
     int reuse = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-        std::cerr << "setsockopt failed\n";
-        return 1;
-    }
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
 
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(6379);
 
-    if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-        std::cerr << "Failed to bind to port 6379\n";
-        return 1;
-    }
-
-    if (listen(server_fd, 5) != 0) {
-        std::cerr << "listen failed\n";
-        return 1;
-    }
+    bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    listen(server_fd, 5);
 
     struct sockaddr_in client_addr;
     int client_addr_len = sizeof(client_addr);
 
-    std::cout << "Waiting for a client to connect...\n";
-
-    // 1. Accept the connection
+    std::cout << "Waiting for a client...\n";
     int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-    if (client_fd < 0) {
-        std::cerr << "Accept failed\n";
-        return 1;
-    }
     std::cout << "Client connected\n";
 
-    // 2. Send the hardcoded PONG response in RESP format
-    const char *response = "+PONG\r\n";
-    send(client_fd, response, strlen(response), 0);
+    // --- START OF STAGE #WY1 LOGIC ---
+    char buffer[1024]; 
+    while (true) {
+        // recv returns the number of bytes read
+        ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+        
+        if (bytes_received <= 0) {
+            // bytes_received == 0 means the client closed the connection
+            // bytes_received < 0 means an error occurred
+            std::cout << "Client disconnected or error occurred.\n";
+            break; 
+        }
 
-    // 3. Clean up
+        // For now, we don't parse the buffer (which contains *1\r\n$4\r\nPING\r\n)
+        // We just hardcode the response for every command received.
+        const char *response = "+PONG\r\n";
+        send(client_fd, response, strlen(response), 0);
+    }
+    // --- END OF STAGE #WY1 LOGIC ---
+
     close(client_fd);
     close(server_fd);
 
