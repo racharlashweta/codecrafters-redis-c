4b0858c2ac0b79eed28fbc5afae398cdd3ed16f9
Shweta Racharla <99076323+racharlashweta@users.noreply.github.com>
2026-04-01 14:26:33 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ std::string to_bulk_string(const std::string& s) {
 }
 
 void handle_client(int client_fd) {
-    char buffer[4096];
+    char buffer[8192]; // Large buffer for heavy LRANGE or large SETs
     while (true) {
         memset(buffer, 0, sizeof(buffer));
         ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
@@ -43,15 +43,19 @@ void handle_client(int client_fd) {
         std::string input(buffer, bytes_received);
         size_t offset = 0;
 
+        // Process all commands in the current buffer
         while (offset < input.size() && input[offset] == '*') {
             try {
-                std::vector<std::string> parts;
                 size_t pos = input.find("\r\n", offset);
+                if (pos == std::string::npos) break;
+                
                 int num_elements = std::stoi(input.substr(offset + 1, pos - offset - 1));
                 offset = pos + 2;
 
+                std::vector<std::string> parts;
                 for (int i = 0; i < num_elements; ++i) {
                     pos = input.find("\r\n", offset);
+                    if (pos == std::string::npos) break;
                     int len = std::stoi(input.substr(offset + 1, pos - offset - 1));
                     offset = pos + 2;
                     parts.push_back(input.substr(offset, len));
@@ -62,9 +66,14 @@ void handle_client(int client_fd) {
                 std::string command = to_lowercase(parts[0]);
                 std::string response = "";
 
+                // --- COMMAND ROUTING ---
+                
                 if (command == "ping") {
                     response = "+PONG\r\n";
                 } 
+                else if (command == "echo" && parts.size() >= 2) {
+                    response = to_bulk_string(parts[1]);
+                }
                 else if (command == "type" && parts.size() >= 2) {
                     std::lock_guard<std::mutex> lock(kv_mutex);
                     if (kv_store.find(parts[1]) == kv_store.end()) response = "+none\r\n";
@@ -74,6 +83,14 @@ void handle_client(int client_fd) {
                     std::lock_guard<std::mutex> lock(kv_mutex);
                     kv_store[parts[1]] = {T_STRING, parts[2], {}};
                     response = "+OK\r\n";
+                }
+                else if (command == "get" && parts.size() >= 2) {
+                    std::lock_guard<std::mutex> lock(kv_mutex);
+                    if (kv_store.count(parts[1]) && kv_store[parts[1]].type == T_STRING) {
+                        response = to_bulk_string(kv_store[parts[1]].string_val);
+                    } else {
+                        response = "$-1\r\n";
+                    }
                 }
                 else if (command == "rpush" || command == "lpush") {
                     {
@@ -145,23 +162,31 @@ void handle_client(int client_fd) {
                             for (int i = start; i <= stop; ++i) response += to_bulk_string(list[i]);
                         }
                     } else response = "*0\r\n";
+                } else {
+                    // Fallback for unknown commands to prevent "no content"
+                    response = "-ERR unknown command\r\n";
                 }
 
                 if (!response.empty()) {
                     send(client_fd, response.c_str(), response.length(), 0);
                 }
-            } catch (...) { break; } // Handle malformed RESP or disconnects
+            } catch (...) { 
+                break; 
+            }
         }
     }
     close(client_fd);
 }
 
 int main() {
+    // Standard setup
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    int reuse = 1; setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    int reuse = 1; 
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
     struct sockaddr_in addr = {AF_INET, htons(6379), INADDR_ANY};
     bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
     listen(server_fd, 5);
+    
     while (true) {
         int cf = accept(server_fd, NULL, NULL);
         if (cf > 0) std::thread(handle_client, cf).detach();
